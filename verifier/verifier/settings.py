8714fd8eac35e23e4724dd0f30ab40bddadc2f74
Ansh Sarkar <anshsark18@gmail.com>
2022-07-31 18:06:26 +0530

    removed secret
    Signed-off-by: Ansh Sarkar <anshsark18@gmail.com>

--- verifier/verifier/settings.py
+++ verifier/verifier/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9(ca%-^$qpw@t+#o2l$bz&ieg1qltr6nzce4w90v&*(&%v1i3s"
+SECRET_KEY = "<Secret Key>"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
